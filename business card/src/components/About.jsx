492461cfd30e39427c32e3b1ab155be0ecf0df0c
zippyzag88 <kuriakosejain456@gmail.com>
2023-08-16 12:30:20 +0530

    about component

---
+++ business card/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About () {
+    return (
+        <div id="about">
+            <h4>About</h4>
+            <p>lorel ipsum lorel ipsum lorel ipsum lorel ipsumlorel ipsumlorel ipsumlorel ipsumlorel ipsumlorel ipsumlorel ipsumlorel ipsumlorel ipsum</p>
+        </div>
+    );
+}
